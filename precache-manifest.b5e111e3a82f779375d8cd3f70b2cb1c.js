6cb499ab8035c939778e6ee9a6c6fc0d98972879
Samuel Gonz <4samuelgg@gmail.com>
2018-12-01 12:22:28 +0000

    primer commit

---
+++ precache-manifest.b5e111e3a82f779375d8cd3f70b2cb1c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "b8c434e1e7cd412df04a",
+    "url": "/static/js/main.b8c434e1.chunk.js"
+  },
+  {
+    "revision": "5e599bcf4900aaf4206a",
+    "url": "/static/js/1.5e599bcf.chunk.js"
+  },
+  {
+    "revision": "b8c434e1e7cd412df04a",
+    "url": "/static/css/main.e3e1edd4.chunk.css"
+  },
+  {
+    "revision": "7614563cc18ef822cd020a4f8584591d",
+    "url": "/index.html"
+  }
+];
